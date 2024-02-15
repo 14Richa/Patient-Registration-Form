d40dcd018b2ee91bcb959551184ba8ce52814898
14Richa <onlinericha19@gmail.com>
2024-02-16 00:01:09 +0530

    added theme stylin

--- src/json.js
+++ src/json.js
@@ -3,6 +3,47 @@ export const json = {
     "logoWidth": "auto",
     "logoHeight": "96",
     "pages": [
+        {
+            "title": "Patient Registration Form",
+            "description": "Your privacy is important to us. All information received through our forms and other communications is subject to our Patient Privacy Policy.",
+    
+            "name": "page1",
+            "elements": [
+              {
+                "type": "panel",
+                "name": "personal-information",
+                "elements": [
+                  {
+                    "type": "text",
+                    "name": "first-name",
+                    "width": "50%",
+                    "minWidth": "256px",
+                    "title": "First Name"
+                  },
+                  {
+                    "type": "text",
+                    "name": "last-name",
+                    "width": "50%",
+                    "minWidth": "256px",
+                    "startWithNewLine": false,
+                    "title": "Last Name"
+                  },
+                  {
+                    "type": "text",
+                    "name": "birthdate",
+                    "width": "50%",
+                    "minWidth": "256px",
+                    "startWithNewLine": false,
+                    "title": "Date of Birth",
+                    "inputType": "date"
+                  },
+                ],
+                "width": "69%",
+                "minWidth": "256px"
+              },
+            
+            ]
+          }
     ],
 
 }

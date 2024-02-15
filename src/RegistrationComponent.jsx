e3f425646f8912a68bc46be694a08b9a7eeacec8
14Richa <onlinericha19@gmail.com>
2024-02-15 16:47:26 +0530

    added first header

---
+++ src/RegistrationComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import { themeJson } from "./theme";
+import "./index.css";
+import { json } from "./json";
+
+function RegistrationComponent() {
+    const survey = new Model(json);
+    survey.applyTheme(themeJson);
+    survey.onComplete.add((sender, options) => {
+        console.log(JSON.stringify(sender.data, null, 3));
+    });
+    return (<Survey model={survey} />);
+}
+
+export default RegistrationComponent;
---
+++ src/json.js
@@ -0,0 +1,8 @@
+export const json = {
+    "logo": "https://raw.githubusercontent.com/14Richa/testga/main/uclhLogo1.png",
+    "logoWidth": "auto",
+    "logoHeight": "96",
+    "pages": [
+    ],
+
+}

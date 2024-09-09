3d4f1953506ab42d53c3389f297be740442bbd2c
14Richa <onlinericha19@gmail.com>
2024-09-09 17:17:47 +0100

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+// src/App.js
 import React from 'react';
+import { Security, LoginCallback } from '@okta/okta-react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FormRenderer from './FormRenderer';
+import oktaConfig from '../oktaConfig'; // Import your Okta config
 
 const App = () => {
   return (
-    <div>
-      <FormRenderer />
-    </div>
+    <Router>
+      <Security {...oktaConfig}>
+        <div>
+          <Switch>
+            <Route path='/callback' component={LoginCallback} />
+            <Route path='/' exact component={FormRenderer} />
+            {/* Add other routes as needed */}
+          </Switch>
+        </div>
+      </Security>
+    </Router>
   );
 };
 

e2dde32ea61b82d572d9a7d4c390b68eab098b60
14Richa <onlinericha19@gmail.com>
2024-09-09 17:50:24 +0100

    added oktaconfig

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Security, LoginCallback } from '@okta/okta-react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FormRenderer from './FormRenderer';
-import oktaConfig from '../oktaConfig';
 
 const App = () => {
   // Check if oktaConfig is loaded
+
+  const oktaConfig = {
+    issuer: 'https://dev-56861500.okta.com/oauth2/default',
+    clientId: '0oajijlhx8pogQNEu5d7',
+    redirectUri: `${window.location.origin}/callback`,
+    scopes: ['openid', 'profile', 'email'],
+  };
   console.log('Okta Config:', oktaConfig);
 
   return (
